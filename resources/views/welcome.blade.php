dc5ec2713fa58c9ab433e5f441e77e22a1458130
hilt <hackerhilt@gmail.com>
2020-12-02 13:45:00 -0300

    Adicionando fonte Adicionando cabeçalho

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,8 @@
         <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet"> 
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
@@ -15,7 +17,11 @@
     </head>
     <body>
         <section class="messages">
-            <ul>
+            <header>
+                <h1 class="title">Messages</h1>
+            </header>
+
+            <ul class="messages-list">
             @foreach ($posts as $post)
                 <li>
                     <p>{{ $post->message }}</p>
